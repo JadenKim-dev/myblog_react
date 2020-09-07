868592d7a11279759e7d384b2d09bd93016c562d
Jaden Kim <jaden.kim5179@gmail.com>
2020-09-07 12:47:57 +0000

    initial commit

---
+++ frontend/src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {BrowserRouter} from "react-router-dom";
+import './index.css';
+import Root from "pages";
+
+ReactDOM.render(
+  <BrowserRouter>
+    <Root />
+  </BrowserRouter>,
+  document.getElementById('root')
+);
+
